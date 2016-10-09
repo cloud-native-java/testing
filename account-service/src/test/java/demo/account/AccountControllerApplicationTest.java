57475b4ba9539568832312b6f2146af8db86300b
Kenny Bastani <kb@socialmoon.com>
2016-10-09 14:38:41 +0200

    Testing travis issue

--- account-service/src/test/java/demo/account/AccountControllerApplicationTest.java
+++ account-service/src/test/java/demo/account/AccountControllerApplicationTest.java
@@ -6,7 +6,6 @@ import demo.user.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -22,9 +21,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
-@AutoConfigureTestDatabase
 public class AccountControllerApplicationTest {
 
     private static final String ACCOUNT_NUMBER = "123456789";
