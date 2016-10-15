49780239b36082886cab355d41538bb57cf2a126
Kenny Bastani <kb@socialmoon.com>
2016-10-15 01:22:19 -0700

    Remove annotation

--- testing-basics/account-service/src/test/java/demo/account/AccountServiceTests.java
+++ testing-basics/account-service/src/test/java/demo/account/AccountServiceTests.java
@@ -5,7 +5,6 @@ import demo.user.UserService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -16,7 +15,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public class AccountServiceTests {
 
     @MockBean
